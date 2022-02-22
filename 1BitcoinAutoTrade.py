2210556d5f87ca75e0958e627a7b903fcb36e5fd
kimmingyu11 <100072014+kimmingyu11@users.noreply.github.com>
2022-02-22 21:59:24 +0900

    Update 1BitcoinAutoTrade.py
    ㄴ

--- 1BitcoinAutoTrade.py
+++ 1BitcoinAutoTrade.py
@@ -42,11 +42,11 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
-        
+
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.4) #k
+            target_price = get_target_price("KRW-BTC", 0.5)
             current_price = get_current_price("KRW-BTC")
-            if target_price < current_price < current_price: #+이동평균선
+            if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
